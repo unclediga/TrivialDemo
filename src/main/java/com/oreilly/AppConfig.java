757d546c7d7f1d4ead17d1d1e0b465030fadb0ce
homeuser <zowie_i@mail.ru>
2020-05-18 15:07:36 +0300

    02_02-JavaBased Configuration

--- src/main/java/com/oreilly/AppConfig.java
+++ src/main/java/com/oreilly/AppConfig.java
@@ -1,14 +1,21 @@
 package com.oreilly;
 
+import com.oreilly.com.oreilly.InfrastructureConfig;
 import com.oreilly.entities.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import javax.sql.DataSource;
 
 @Configuration
+@Import(InfrastructureConfig.class)
 public class AppConfig {
     @Bean
-    public Game game(){
-        return new BaseballGame(redSox(), cubs());
+    public Game game(DataSource dataSource){
+        final BaseballGame baseballGame = new BaseballGame(redSox(), cubs());
+        baseballGame.setDataSource(dataSource);
+        return baseballGame;
     }
 
     @Bean
